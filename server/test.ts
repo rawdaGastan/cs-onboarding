c4db9cc5a9193e0f1ae5b097d98eabf1f8992f13
rawdaGastan <rody.rawda.rr@gmail.com>
2022-05-18 01:26:45 +0200

    test

---
+++ server/test.ts
@@ -0,0 +1,67 @@
+const axios = require('axios');
+
+test('Add numbers 33 + 9 = 42', async() => {
+    const res = await axios.post('http://localhost:3000/calc/', {
+        "firstValue": 33,
+        "operator": "+",
+        "secondValue": 9
+    });
+
+    var answer = res.data.answer;
+    expect(answer).toBe(42);
+})
+
+test('Subtract numbers 33 - 9 = 24', async() => {
+    const res = await axios.post('http://localhost:3000/calc/', {
+        "firstValue": 33,
+        "operator": "-",
+        "secondValue": 9
+    });
+
+    var answer = res.data.answer;
+    expect(answer).toBe(24);
+})
+
+test('Divide numbers 36 / 6 = 6', async() => {
+    const res = await axios.post('http://localhost:3000/calc/', {
+        "firstValue": 36,
+        "operator": "/",
+        "secondValue": 6
+    });
+
+    var answer = res.data.answer;
+    expect(answer).toBe(6);
+})
+
+test('Multiply numbers 6 * 6 = 36', async() => {
+    const res = await axios.post('http://localhost:3000/calc/', {
+        "firstValue": 6,
+        "operator": "*",
+        "secondValue": 6
+    });
+
+    var answer = res.data.answer;
+    expect(answer).toBe(36);
+})
+
+test('Remainder numbers 6 % 4 = 2', async() => {
+    const res = await axios.post('http://localhost:3000/calc/', {
+        "firstValue": 6,
+        "operator": "%",
+        "secondValue": 4
+    });
+
+    var answer = res.data.answer;
+    expect(answer).toBe(2);
+})
+
+test('Power numbers 6 ^ 2 = 36', async() => {
+    const res = await axios.post('http://localhost:3000/calc/', {
+        "firstValue": 6,
+        "operator": "^",
+        "secondValue": 2
+    });
+
+    var answer = res.data.answer;
+    expect(answer).toBe(36);
+})
