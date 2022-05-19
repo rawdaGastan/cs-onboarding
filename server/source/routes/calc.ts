64a0b41d346e9c9cb9b5c2fb55e4b58c0289495e
rawdaGastan <rody.rawda.rr@gmail.com>
2022-05-19 11:59:44 +0200

    testing

--- server/source/routes/calc.ts
+++ server/source/routes/calc.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import controller from '../controllers/calc';
+
 const router = express.Router();
 
 router.post('/calc', (req, res, next) => {
@@ -8,7 +9,7 @@ router.post('/calc', (req, res, next) => {
     const secondValue: number = req.body.secondValue;
 
     const result: number = controller.calc(firstValue, operator, secondValue);
-    
+
     res.send({ answer: result });
     next();
 });
--- server/source/test.ts
+++ server/source/test.ts
@@ -1,6 +1,38 @@
-const axios = require('axios');
+//const axios = require('axios');
+import controller from './controllers/calc';
 
+test('Add numbers 33 + 9 = 42', async() => {
+    var answer = controller.calc(33, "+", 9);
+    expect(answer).toBe(42);
+})
 
+test('Subtract numbers 33 - 9 = 24', async() => {
+    var answer = controller.calc(33, "-", 9);
+    expect(answer).toBe(24);
+})
+
+test('Divide numbers 36 / 6 = 6', async() => {
+    var answer = controller.calc(36, "/", 6);
+    expect(answer).toBe(6);
+})
+
+test('Multiply numbers 6 * 6 = 36', async() => {
+    var answer = controller.calc(6, "*", 6);
+    expect(answer).toBe(36);
+})
+
+test('Remainder numbers 6 % 4 = 2', async() => {
+    var answer = controller.calc(6, "%", 4);
+    expect(answer).toBe(2);
+})
+
+test('Power numbers 6 ^ 2 = 36', async() => {
+    var answer = controller.calc(6, "^", 2);
+    expect(answer).toBe(36);
+})
+
+// local test
+/*
 test('Add numbers 33 + 9 = 42', async() => {
     const res = await axios.post('http://0.0.0.0:3000/calc/', {
         "firstValue": 33,
@@ -66,3 +98,4 @@ test('Power numbers 6 ^ 2 = 36', async() => {
     var answer = res.data.answer;
     expect(answer).toBe(36);
 })
+*/
